4e6c5b28bffd86654ff773a2a5eed0cb7dbf86e6
Sheludko Oleg <sheludko.c.s@gmail.com>
2023-04-12 15:43:41 +0300

    fixed bugs

--- src/Components/Track/Track.jsx
+++ src/Components/Track/Track.jsx
@@ -1,18 +1,17 @@
-import React from "react";
+import React, { useMemo } from "react";
 import './track.css';
 
 function Track({ track, isRemoval, onAdd, onRemove }) {
-  const addTrack = () => onAdd(track);
-  const removeTrack = () => onRemove(track);
+  const renderAction = useMemo(() => {
+    const addTrack = () => onAdd(track);
+    const removeTrack = () => onRemove(track);
 
-  function renderAction() {
     if (isRemoval) {
       return <button className="trackAction" onClick={removeTrack}>-</button>;
     }
-    return (
-      <button className="trackAction" onClick={addTrack}>+</button>
-    );
-  }
+    return <button className="trackAction" onClick={addTrack}>+</button>;
+  }, [isRemoval, onAdd, onRemove, track]);
+
 
   return (
     <div className="track">
